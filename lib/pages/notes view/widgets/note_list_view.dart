f2532abfc0c12367118659e8dc1ca784b6c77af0
mousa <ahmed56692523@gmail.com>
2026-08-27 18:30:45 +0300

    note list view

---
+++ lib/pages/notes view/widgets/note_list_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/pages/notes%20view/widgets/notes_item.dart';
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, index) {
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 16.0),
+        child: NoteItem(),
+      );
+    });
+  }
+}
--- lib/pages/notes view/widgets/notes_view_body.dart
+++ lib/pages/notes view/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/pages/notes%20view/widgets/custom_appbar.dart';
+import 'package:to_do_app/pages/notes%20view/widgets/note_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -9,67 +10,9 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
-        children: [SizedBox(height: 50), const CustomAppbar(), NoteItem()],
+        children: [SizedBox(height: 50), const CustomAppbar(), Expanded(child: NoteListView())],
       ),
     );
   }
 }
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 229, 161, 60),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 24.0, bottom: 24),
-              child: ListTile(
-                title: Text(
-                  "Ahmed mousa",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.only(top: 10.0, bottom: 10),
-                  child: Text(
-                    "flutter dev in hdk in qater",
-                    style: TextStyle(
-                      fontSize: 19,
-                      color: Colors.black.withValues(alpha: 0.5),
-                    ),
-                  ),
-                ),
-                trailing: Icon(
-                  Icons.delete_rounded,
-                  color: Colors.black,
-                  size: 30,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 28.0, left: 225),
-              child: Text(
-                "may 21,2024",
-                style: TextStyle(
-                  fontSize: 19,
-                  color: Colors.black.withValues(alpha: 0.5),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
