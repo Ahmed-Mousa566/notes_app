051101e7e5cb5abf21cd27a353e944be0221d628
mousa <ahmed56692523@gmail.com>
2026-08-29 14:57:16 +0300

    add floatingActionButton

--- lib/pages/notes view/widgets/note_list_view.dart
+++ lib/pages/notes view/widgets/note_list_view.dart
@@ -6,11 +6,17 @@ class NoteListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return Padding(
-        padding: const EdgeInsets.only(bottom: 16.0),
-        child: NoteItem(),
-      );
-    });
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ListView.builder(padding: EdgeInsets.zero,
+      itemBuilder: (context, index)
+       {
+        
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: NoteItem(),
+        );
+      }),
+    );
   }
 }
